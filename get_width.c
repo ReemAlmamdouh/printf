da548f06c62872849c5623aee74852e7ef723d6f
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-03-26 21:20:08 -0700

    get_width File

---
+++ get_width.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * get_width - A function that calculates the width
+ * @format:  Strings format in which to print the arguments
+ * @i: Printed Arguments List
+ * @argumentList: List of arguments
+ *
+ * Return: width
+ *
+ * Author: Ahmed Abdel Nasser & Reem AlMamdouh
+ * ALX Software Engineering Cohort 12
+ */
+int get_width(const char *format, int *i, va_list aList)
+{
+	int width = 0;
+	int curr_i;
+
+	for (curr_i = *i + 1 ; format[curr_i] != '\0' ; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			width = width * 10;
+			width = with + format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+		{
+			break;
+		}
+	}
+	*i = *curr_i - 1;
+}
